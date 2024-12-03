a8f0b1a2e7a0a65aa76861e6543c186ee9d194d5
Ali AlWahayb <alialwahayb@outlook.com>
2024-12-03 19:53:18 +0300

    UI Edits

--- app/src/main/java/com/example/heatstrokealertapp/MainActivity.java
+++ app/src/main/java/com/example/heatstrokealertapp/MainActivity.java
@@ -1,2 +1,17 @@
 package com.example.heatstrokealertapp;
 
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Display a Toast message as a basic action
+        Toast.makeText(this, "Welcome to HeatStrokeAlertApp", Toast.LENGTH_LONG).show();
+    }
+}
